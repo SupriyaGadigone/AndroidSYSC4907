31b2aeaf0d13e999aade9c85a8711062fc6fdee6
Supriya Gadigone <supriyagadigone@hotmail.com>
2018-11-04 11:39:30 -0500

    Implemented Read from NFC

--- app/src/main/java/com/example/supriyagadigone/androidsysc4907/CustomerReadNfc.java
+++ app/src/main/java/com/example/supriyagadigone/androidsysc4907/CustomerReadNfc.java
@@ -1,14 +1,70 @@
 package com.example.supriyagadigone.androidsysc4907;
 
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Reference: https://www.codexpedia.com/android/android-nfc-read-and-write-example/
+ */
 public class CustomerReadNfc extends AppCompatActivity {
+
+    private Context mContext;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_read_nfc_page);
+
+        mContext = this;
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this.mContext, "This deivice doesnt support NFC", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        readFromIntent(getIntent());
+
+    }
+
+    private void readFromIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+        int languageCodeLength = payload[0] & 0063;
+
+        try {
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+
     }
 
 }
