ed3e0fe4158c00db1afcaecbbceafc2025a6f837
Supriya Gadigone <supriyagadigone@hotmail.com>
2018-11-04 11:09:39 -0500

    Added Customer Read NFC Page

--- app/src/main/java/com/example/supriyagadigone/androidsysc4907/CustomerLandingPage.java
+++ app/src/main/java/com/example/supriyagadigone/androidsysc4907/CustomerLandingPage.java
@@ -1,14 +1,27 @@
 package com.example.supriyagadigone.androidsysc4907;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomerLandingPage extends AppCompatActivity {
+
+    private Button mReadNfcButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_landing_page_activity);
+
+        mReadNfcButton = findViewById(R.id.customer_read_nfc_button);
+        mReadNfcButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent quizIntent = new Intent(CustomerLandingPage.this, CustomerReadNfc.class);
+                startActivity(quizIntent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/supriyagadigone/androidsysc4907/CustomerReadNfc.java
@@ -0,0 +1,14 @@
+package com.example.supriyagadigone.androidsysc4907;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CustomerReadNfc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_read_nfc_page);
+    }
+
+}
