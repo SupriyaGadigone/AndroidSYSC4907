a574dd0bdd3efd97ae49f974b9cd3e3d0b707ee4
Supriya Gadigone <supriyagadigone@hotmail.com>
2018-11-04 10:45:17 -0500

    Added Write and Edit NFC Page (last commit message was wrong)

---
+++ app/src/main/java/com/example/supriyagadigone/androidsysc4907/OrgEditNfc.java
@@ -0,0 +1,14 @@
+package com.example.supriyagadigone.androidsysc4907;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrgEditNfc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.org_edit_nfc_page);
+
+    }
+}
--- app/src/main/java/com/example/supriyagadigone/androidsysc4907/OrgLandingPage.java
+++ app/src/main/java/com/example/supriyagadigone/androidsysc4907/OrgLandingPage.java
@@ -9,18 +9,27 @@ import android.widget.Button;
 public class OrgLandingPage extends AppCompatActivity {
 
     private Button mWriteNFCButton;
+    private Button mEditNFCButton;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.org_landing_page_activity);
 
-            mWriteNFCButton = findViewById(R.id.org_write_nfc_button);
-            mWriteNFCButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Intent quizIntent = new Intent(OrgLandingPage.this, OrgWriteNfc.class);
-                    startActivity(quizIntent);
-                }
-            });
+        mWriteNFCButton = findViewById(R.id.org_write_nfc_button);
+        mWriteNFCButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent quizIntent = new Intent(OrgLandingPage.this, OrgWriteNfc.class);
+                startActivity(quizIntent);
+            }
+        });
+
+        mEditNFCButton = findViewById(R.id.org_edit_nfc_button);
+        mEditNFCButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent quizIntent = new Intent(OrgLandingPage.this, OrgEditNfc.class);
+                startActivity(quizIntent);
+            }
+        });
     }
 }
