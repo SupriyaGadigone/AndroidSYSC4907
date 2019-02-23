655e95831e2713e271dc490b7f3ddfb08d70bdc5
Supriya Gadigone <supriyagadigone@hotmail.com>
2019-02-23 11:25:52 -0500

    Set up grocery list, stirng

--- app/src/main/java/com/example/supriyagadigone/androidsysc4907/Customer/CustomerGroceryList.java
+++ app/src/main/java/com/example/supriyagadigone/androidsysc4907/Customer/CustomerGroceryList.java
@@ -35,11 +35,11 @@ public class CustomerGroceryList extends BaseActivity implements OnResponseCallb
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView lvItems;
-    private String  listData;
+    private String listData;
     private Button mAddBtn;
     private Map<String, String> productData;
     private String listId;
-
+    private String orgId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +65,7 @@ public class CustomerGroceryList extends BaseActivity implements OnResponseCallb
         listData = getIntent().getStringExtra("LIST_DATA");
         Log.e(TAG, "********: " + listData);
 
-        if(listData!=null) {
+        if (listData != null) {
             parseListData(listData);
         }
 
@@ -74,6 +74,7 @@ public class CustomerGroceryList extends BaseActivity implements OnResponseCallb
                 "productList");
 
         setupListViewListener();
+        setupListViewListenerOnClick();
     }
 
     public void onAddItem(View v) {
@@ -107,28 +108,46 @@ public class CustomerGroceryList extends BaseActivity implements OnResponseCallb
                 });
     }
 
-    private void parseListData(String response){
-    //    Log.e(TAG, "parseListData(): "+ response);
+    // Attaches a long click listener to the listview
+    private void setupListViewListenerOnClick() {
+//TODO
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = lvItems.getItemAtPosition(position).toString();
+                if(lvItems.getItemAtPosition(position).toString().contains("IN STOCK")){
+                    Map<String, String> prodInfo = new HashMap<>();
+//                    prodInfo.put("nfc_id", );
+//                    RequestHandler mRequestHandlerm = new RequestHandler(getApplicationContext(),
+//                            CustomerGroceryList.this,
+//                            "product", prodInfo);
+                }
+            }
+        });
+    }
+
+    private void parseListData(String response) {
+        //    Log.e(TAG, "parseListData(): "+ response);
         try {
             JSONObject shoppingListData = new JSONObject(response);
             JSONArray shoppingListsArray = new JSONArray(shoppingListData.getString("product"));
             listId = shoppingListData.getString("list_id");
-            for(int i = 0; i < shoppingListsArray.length(); i++) {
+            for (int i = 0; i < shoppingListsArray.length(); i++) {
                 JSONObject shoppingList = new JSONObject(shoppingListsArray.get(i).toString());
                 items.add(shoppingList.getString("name"));
-
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    private void setSearchData(String response){
+    private void setSearchData(String response) {
         Log.e(TAG, response);
         List<String> productsList = new ArrayList<String>();
         //List<String> productsList = new ArrayList<String>();
         try {
-        JSONArray jsonData = new JSONArray(response);
+            JSONArray jsonData = new JSONArray(response);
             try {
                 for (int i = 0; i < jsonData.length(); i++) {
                     JSONObject productJsonObj = new JSONObject(jsonData.get(i).toString());
@@ -152,33 +171,35 @@ public class CustomerGroceryList extends BaseActivity implements OnResponseCallb
         setListNfcData();
 
     }
+
     public void onResponse(String endpoint, String response) {
-        Log.e(TAG,"respp: "+ response);
-        if (endpoint.equals("shoppingList") && response!=""){
+        Log.e(TAG, "respp: " + response);
+        if (endpoint.equals("shoppingList") && response != "") {
             setNfcData(response);
         }
 
-        if(endpoint.equals("productList")){
+        if (endpoint.equals("productList")) {
             setSearchData(response);
         }
 
     }
 
-    private void setNfcData(String response){
-        Log.e(TAG,"setNfcData: "+ response);
+    private void setNfcData(String response) {
+        Log.e(TAG, "setNfcData: " + response);
         try {
             JSONArray jsonData = new JSONArray(response);
 
-            for(int i = 0; i < jsonData.length(); i++){
+            for (int i = 0; i < jsonData.length(); i++) {
                 JSONObject prodData = new JSONObject(jsonData.get(i).toString());
                 JSONObject productnfcData = new JSONObject(prodData.getString("product"));
-                Log.e(TAG, "Name: "+ productnfcData.getString("name"));
-                Log.e(TAG, "nfcid: "+ prodData.getString("nfc_id"));
-                if(!(prodData.getString("nfc_id").equals("False"))){
-                    Log.e(TAG, "nameee: "+items.indexOf(productnfcData.getString("name")));
+                Log.e(TAG, "Name: " + productnfcData.getString("name"));
+                Log.e(TAG, "nfcid: " + prodData.getString("nfc_id"));
+                if (!(prodData.getString("nfc_id").equals("False"))) {
+                    Log.e(TAG, "nameee: " + items.indexOf(productnfcData.getString("name")));
                     int index = items.indexOf(productnfcData.getString("name"));
                     items.remove(index);
-                    items.add(productnfcData.getString("name") + " [IN STOCK]");
+                    items.add(productnfcData.getString("name") + " [IN STORE]");
+                    //TODO: add nfc id to a map and then on click get it, make a new one each time
                 }
             }
             itemsAdapter.notifyDataSetChanged();
@@ -187,28 +208,27 @@ public class CustomerGroceryList extends BaseActivity implements OnResponseCallb
         }
 
 
-
     }
 
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         Intent intent = new Intent(this, CustomerGroceryListPage.class);
         startActivity(intent);
     }
 
-    private void setListNfcData(){
+    private void setListNfcData() {
         Map<String, String> storeProdInfo = new HashMap<>();
 
         SharedPreferences prefs = getApplicationContext().getSharedPreferences(LoginActivity.LOGIN_PREFS_NAME, Context.MODE_PRIVATE);
         Map<String, String> allEntries = (Map<String, String>) prefs.getAll();
 
         for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
-            Log.e(TAG,"key: " +entry.getKey());
+            Log.e(TAG, "key: " + entry.getKey());
             if (entry.getKey().equals("org_id")) {
                 storeProdInfo.put("org_id", entry.getValue().toString());
-            } else
-            {
+                orgId = entry.getValue().toString();
+            } else {
                 storeProdInfo.put("org_id", "001");
+                orgId = "001";
             }
         }
 
